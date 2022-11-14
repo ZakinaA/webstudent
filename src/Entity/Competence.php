fbe262f6592bbb919a8479592ee2163a98e11ce1
zakinaA <zakina.rostand@gmail.com>
2022-11-14 14:35:02 +0100

    ajout propriété nomLong dans entité Competence #see #1

---
+++ src/Entity/Competence.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CompetenceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CompetenceRepository::class)]
+class Competence
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nomLong = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomLong(): ?string
+    {
+        return $this->nomLong;
+    }
+
+    public function setNomLong(string $nomLong): self
+    {
+        $this->nomLong = $nomLong;
+
+        return $this;
+    }
+}
